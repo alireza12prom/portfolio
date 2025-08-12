ea3b89e3c1d359998ee61948247a2042293c1a6f
alireza.rezapour <alireza.rezapour.me@gmail.com>
2025-08-12 17:06:42 +0330

    add about me, experience and skill sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-[#ffffff] text-[#000000]">
+    <main className="container relative mx-auto overflow-auto md:p-16 p-4 bg-[#ffffff] text-[#000000]">
 
-      <section>
+      <section className="flex flex-col items-center space-y-8">
 
         {/* Header */}
-        <div className="flex items-stretch justify-center h-max gap-20 p-2 border-2  border-gray-600 border-dashed">
+        <div className="flex items-stretch justify-between p-2 w-3xl border-gray-600">
 
           <div className="flex flex-col space-y-1.5">
             {/* General */}
@@ -72,6 +72,67 @@ export default function Home() {
           </div>
         </div>
 
+
+        {/* About Me */}
+        <section className="flex flex-col items-stretch justify-center p-2 w-3xl space-y-1.5 border-gray-600 border-2 border-dashed">
+          <h1 className="text-xl">About Me</h1>
+          <p className="text-black/80">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit natus fugit nobis! Rerum alias dignissimos assumenda atque eligendi rem animi quas ipsam ab? Voluptatem minima pariatur blanditiis, vero necessitatibus nemo?</p>
+        </section>
+
+
+        {/* Experience */}
+        <section className="flex flex-col items-stretch justify-center p-2 w-3xl space-y-1.5 border-gray-600 border-2 border-dashed">
+          <h1 className="text-xl">Work Experience</h1>
+
+          <div>
+            <div className="flex items-stretch justify-between">
+              <h3>Part Software Group</h3>
+              <p className="text-sm text-gray-500">
+                Dec 2022
+                -
+                Feb 2024
+              </p>
+            </div>
+
+            <h4 className="font-mono text-sm text-gray-500">Software Engineer • Fulltime</h4>
+
+            <div className="flex flex-col items-stretch ml-3">
+              <p className="text-sm">⁃ Option 1</p>
+              <p className="text-sm">⁃ Option 2</p>
+              <p className="text-sm">⁃ Option 3</p>
+              <p className="text-sm">⁃ Option 4</p>
+            </div>
+          </div>
+        </section>
+
+
+        {/* Skills */}
+        <section className="flex flex-col gap-3 justify-center p-2 w-3xl space-y-1.5 border-gray-600 border-2 border-dashed">
+          <h3>Skills</h3>
+
+          <div className="flex flex-wrap gap-2 text-sm">
+            <p>Languages & Frameworks:</p>
+
+            {
+              ["javascript", "typescript", "node.js", "golang", "express.js", "nest.js", "fastify"]
+                .map((item) => {
+                  return <div className="px-1.5 py-0.5 bg-gray-900 text-white rounded hover:bg-gray-600">{item}</div>
+                })
+            }
+          </div>
+
+          <div className="flex flex-wrap gap-2 text-sm">
+            <p>Databases:</p>
+
+            {
+              ["redis", "postgresql", "mysql", "mongodb"]
+                .map((item) => {
+                  return <div className="px-1.5 py-0.5 text-sm bg-gray-900 text-white rounded hover:bg-gray-600">{item}</div>
+                })
+            }
+          </div>
+
+        </section>
       </section >
 
     </main >
